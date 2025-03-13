e3363b97fbca3bfa59be2d449b6d838fdec21a18
JOHN CLARKE <JCLARKE@ie.ibm.com>
2025-03-13 11:41:23 +0000

    Update tables_v01.0003_to_v01.0004.sql

--- ScenarioLaunchPlatform/database/ScenarioLaunchPlatform_CORE/patches/tables_v01.0003_to_v01.0004.sql
+++ ScenarioLaunchPlatform/database/ScenarioLaunchPlatform_CORE/patches/tables_v01.0003_to_v01.0004.sql
@@ -4,21 +4,21 @@ BEGIN
 RAISE NOTICE 'Running patch';
 
 ALTER TABLE tb_databaseconnections
-ADD COLUMN connection_alias character varying(500);
+ADD COLUMN db_alias character varying(500);
 
 ALTER TABLE tb_databaseconnections
-ADD COLUMN connection_access character varying(500);
+ADD COLUMN db_access character varying(500);
 
 
-UPDATE tb_databaseconnections SET connection_alias = 'Standard User',connection_access = 'Select,Update,Insert,Delete,Execute' where db_type = 'mysql' and db_username = 'JOHN';
-UPDATE tb_databaseconnections SET connection_alias = 'Standard User',connection_access = 'Select,Update,Insert,Delete,Execute' where db_type = 'mysql' and db_username = 'JASON';
-UPDATE tb_databaseconnections SET connection_alias = 'DB Admin',connection_access = 'All Privileges' where db_type = 'mysql' and db_username = 'POLLY';
-UPDATE tb_databaseconnections SET connection_alias = 'Badguy',connection_access = 'All Privileges' where db_type = 'mysql' and db_username = 'LIHER';
+UPDATE tb_databaseconnections SET db_alias = 'Standard User',db_access = 'Select,Update,Insert,Delete,Execute' where db_type = 'mysql' and db_username = 'JOHN';
+UPDATE tb_databaseconnections SET db_alias = 'Standard User',db_access = 'Select,Update,Insert,Delete,Execute' where db_type = 'mysql' and db_username = 'JASON';
+UPDATE tb_databaseconnections SET db_alias = 'DB Admin',db_access = 'All Privileges' where db_type = 'mysql' and db_username = 'POLLY';
+UPDATE tb_databaseconnections SET db_alias = 'Badguy',db_access = 'All Privileges' where db_type = 'mysql' and db_username = 'LIHER';
 
-UPDATE tb_databaseconnections SET connection_alias = 'Standard User',connection_access = 'Select,Update,Insert,Delete,Usage' where db_type = 'postgresql' and db_username = 'john';
-UPDATE tb_databaseconnections SET connection_alias = 'Standard User',connection_access = 'Select,Update,Insert,Delete,Usage' where db_type = 'postgresql' and db_username = 'jason';
-UPDATE tb_databaseconnections SET connection_alias = 'DB Admin',connection_access = 'Superuser' where db_type = 'postgresql' and db_username = 'polly';
-UPDATE tb_databaseconnections SET connection_alias = 'Badguy',connection_access = 'Superuser' where db_type = 'postgresql' and db_username = 'liher';
+UPDATE tb_databaseconnections SET db_alias = 'Standard User',db_access = 'Select,Update,Insert,Delete,Usage' where db_type = 'postgresql' and db_username = 'john';
+UPDATE tb_databaseconnections SET db_alias = 'Standard User',db_access = 'Select,Update,Insert,Delete,Usage' where db_type = 'postgresql' and db_username = 'jason';
+UPDATE tb_databaseconnections SET db_alias = 'DB Admin',db_access = 'Superuser' where db_type = 'postgresql' and db_username = 'polly';
+UPDATE tb_databaseconnections SET db_alias = 'Badguy',db_access = 'Superuser' where db_type = 'postgresql' and db_username = 'liher';
 
 
 
