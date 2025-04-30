774426b1c3b663058999354d195344dde6ec885e
jasoneflood <jasoneflood@gmail.com>
2025-04-30 10:35:19 +0100

    Update tables.sql

--- ScenarioLaunchPlatform/database/ScenarioLaunchPlatform_CORE/tables.sql
+++ ScenarioLaunchPlatform/database/ScenarioLaunchPlatform_CORE/tables.sql
@@ -22,9 +22,16 @@ CREATE TABLE IF NOT EXISTS public.tb_version
 (
     version character varying(100)
 );
-insert into public.tb_version(version) VALUES('v01.0005');
+insert into public.tb_version(version) VALUES('v01.0007');
 
+-- 1.0007 changes --
+CREATE TABLE tb_myvars 
+(
+	id SERIAL PRIMARY KEY, username VARCHAR(255), data JSONB
+);
 
+INSERT INTO tb_myvars (username, data) VALUES ('admin','{"my_var1": 1, "my_var2": "simpleString1"}');
+INSERT INTO tb_myvars (username, data) VALUES ('username','{"my_var1": 2, "my_var2": "simpleString2"}');
 
 
 CREATE TABLE IF NOT EXISTS public.tb_stories
