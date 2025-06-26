7568b8b2612196ba544ab7b87aa3a1c28eedaa1f
JOHN CLARKE <JCLARKE@ie.ibm.com>
2025-06-26 20:52:02 +0100

    updates to main schema

--- ScenarioLaunchPlatform/database/ScenarioLaunchPlatform_CORE/tables.sql
+++ ScenarioLaunchPlatform/database/ScenarioLaunchPlatform_CORE/tables.sql
@@ -108,12 +108,18 @@ CREATE TABLE IF NOT EXISTS public.tb_content_packs
 (
     id SERIAL PRIMARY KEY,
     pack_name character varying(255),
-    pack_file_path text NOT NULL,
-	pack_output_path text NOT NULL,
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    version character varying(255),
+	db_type character varying(255),
+	build_date character varying(255),
+	build_version character varying(255),
+	description character varying(255),
+	author character varying(255),
+	icon character varying(255),
+	background_traffic character varying(255),
     pack_file_content bytea,
 	pack_info jsonb,
-	pack_deployed character varying(5) DEFAULT 'false'
+	pack_deployed character varying(5) DEFAULT 'false',
+	uploaded_date timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
 
 RAISE NOTICE 'Created tb_content_packs';
