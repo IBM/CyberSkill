171909780fd14a5b8e4845a4fa0e14d19b7b7b10
JohnClarke <2584883+JCFL-Dev@users.noreply.github.com>
2025-07-01 01:54:54 +0100

    Update 1.0009_toLatest.sql

--- ScenarioLaunchPlatform/database/ScenarioLaunchPlatform_CORE/patches/1.0009_toLatest.sql
+++ ScenarioLaunchPlatform/database/ScenarioLaunchPlatform_CORE/patches/1.0009_toLatest.sql
@@ -4,12 +4,12 @@
 CREATE TABLE IF NOT EXISTS public.tb_content_packs
 (
     id SERIAL PRIMARY KEY,
-    pack_name character varying(255),
-    version character varying(255),
-	db_type character varying(255),
+    pack_name character varying(255) NOT NULL,
+    version character varying(255) NOT NULL,
+	db_type character varying(255) NOT NULL,
 	build_date character varying(255),
-	build_version character varying(255),
-	description character varying(255),
+	build_version character varying(255) NOT NULL,
+	description character varying(255) NOT NULL,
 	author character varying(255),
 	icon character varying(255),
 	background_traffic character varying(255),
@@ -17,7 +17,8 @@ CREATE TABLE IF NOT EXISTS public.tb_content_packs
 	pack_info jsonb,
 	pack_deployed character varying(5) DEFAULT 'false',
 	uploaded_date timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+	 -- Unique constraint on pack_name + version
+    CONSTRAINT unique_pack_version UNIQUE (pack_name, version)
 );
-
 
 UPDATE public.tb_version SET version = 'v01.0010';
