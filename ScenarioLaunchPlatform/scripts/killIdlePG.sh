fdf79e9fe9a11e8415ca8e630065087e84e869fa
JOHN CLARKE <JCLARKE@ie.ibm.com>
2025-09-04 16:31:09 +0100

    killPG

--- ScenarioLaunchPlatform/scripts/killIdlePG.sh
+++ ScenarioLaunchPlatform/scripts/killIdlePG.sh
@@ -12,7 +12,7 @@ IDLE_MINUTES=1            # Only kill if idle for more than X minutes
 # --- EXECUTION ---
 echo "Finding idle PostgreSQL connections older than ${IDLE_MINUTES} minutes..."
 
-psql -U "$PGUSER" -d "$PGDATABASE" -h "$PGSOCKET" -Atc "
+sudo -u postgres psql <<EOF
 SELECT pg_terminate_backend(pid)
 FROM pg_stat_activity
 WHERE state = 'idle'
@@ -20,4 +20,5 @@ WHERE state = 'idle'
   AND usename = 'postgres'
   AND client_addr = '127.0.0.1'
   AND pid <> pg_backend_pid();
-  "
+
+EOF
